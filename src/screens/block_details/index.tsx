402b17f557fe99bc85291d0089036b6ccc048da8
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-11-11 16:01:40 +0000

    Replace tabs with spaces

--- src/screens/block_details/index.tsx
+++ src/screens/block_details/index.tsx
@@ -2,60 +2,60 @@ import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import { NextSeo } from 'next-seo';
 import {
-	Layout,
-	LoadAndExist,
+    Layout,
+    LoadAndExist,
 } from '@components';
 import {
-	Overview,
-	Transactions,
-	Signatures,
+    Overview,
+    Transactions,
+    Signatures,
 } from './components';
 import { useStyles } from './styles';
 import { useBlockDetails } from './hooks';
 
 const BlockDetails = () => {
-	const { t } = useTranslation('blocks');
-	const classes = useStyles();
-	const { state } = useBlockDetails();
-	const {
-		overview,
-		signatures,
-		transactions,
-	} = state;
+    const { t } = useTranslation('blocks');
+    const classes = useStyles();
+    const { state } = useBlockDetails();
+    const {
+        overview,
+        signatures,
+        transactions,
+    } = state;
 
-	return (
-		<>
-			<NextSeo
-				title={t('blockDetails')}
-				openGraph={{
-					title: t('blockDetails'),
-				}}
-			/>
-			<Layout navTitle={t('blockDetails')}>
-				<LoadAndExist
-					loading={state.loading}
-					exists={state.exists}
-				>
-					<span className={classes.root}>
-						<Overview
-							height={overview.height}
-							hash={overview.hash}
-							proposer={overview.proposer}
-							timestamp={overview.timestamp}
-							txs={overview.txs}
-						/>
-						<Signatures
-							className={classes.signatures}
-							signatures={signatures}
-						/>
-						<Transactions
-							transactions={transactions}
-						/>
-					</span>
-				</LoadAndExist>
-			</Layout>
-		</>
-	);
+    return (
+        <>
+            <NextSeo
+                title={t('blockDetails')}
+                openGraph={{
+                    title: t('blockDetails'),
+                }}
+            />
+            <Layout navTitle={t('blockDetails')}>
+                <LoadAndExist
+                    loading={state.loading}
+                    exists={state.exists}
+                >
+                    <span className={classes.root}>
+                        <Overview
+                            height={overview.height}
+                            hash={overview.hash}
+                            proposer={overview.proposer}
+                            timestamp={overview.timestamp}
+                            txs={overview.txs}
+                        />
+                        <Signatures
+                            className={classes.signatures}
+                            signatures={signatures}
+                        />
+                        <Transactions
+                            transactions={transactions}
+                        />
+                    </span>
+                </LoadAndExist>
+            </Layout>
+        </>
+    );
 };
 
 export default BlockDetails;
--- src/screens/transaction_details/components/messages/index.test.tsx
+++ src/screens/transaction_details/components/messages/index.test.tsx
@@ -9,47 +9,47 @@ import Messages from '.';
 // mocks
 // ==================================
 jest.mock('@components', () => ({
-	Box: (props) => <div id="Box" {...props} />,
-	Tag: (props) => <div id="Tag" {...props} />,
-	TransactionMessagesFilter: (props) => <div id="TransactionMessagesFilter" {...props} />,
+    Box: (props) => <div id="Box" {...props} />,
+    Tag: (props) => <div id="Tag" {...props} />,
+    TransactionMessagesFilter: (props) => <div id="TransactionMessagesFilter" {...props} />,
 }));
 
 jest.mock('react-virtualized-auto-sizer', () => ({ children }: any) => children({
-	height: 600, width: 600,
+    height: 600, width: 600,
 }));
 
 // ==================================
 // unit tests
 // ==================================
 describe('screen: TransactionDetails/ Messages', () => {
-	it('matches snapshot', () => {
-		const component = renderer.create(
-			<RecoilRoot>
-				<MockTheme>
-					<Messages
-						messages={[MsgUnknown.fromJSON({
-							'@type': '/cosmos.bank.v1beta1.MsgSend',
-							amount: [
-								{
-									denom: 'udaric',
-									amount: '1100000',
-								},
-							],
-							to_address: 'desmos1srujv22zfrwyfvu2vyyaqqq3f0z7yjeaggd9n2',
-							from_address: 'desmos1dzn2s7l0wm9kekyazcnhapu8j95n90efmcmrad',
-						})]}
-						viewRaw={false}
-						toggleMessageDisplay={jest.fn()}
-						onMessageFilterCallback={jest.fn()}
-					/>
-				</MockTheme>
-			</RecoilRoot>,
-		);
-		const tree = component.toJSON();
-		expect(tree).toMatchSnapshot();
-	});
+    it('matches snapshot', () => {
+        const component = renderer.create(
+            <RecoilRoot>
+                <MockTheme>
+                    <Messages
+                        messages={[MsgUnknown.fromJSON({
+                            '@type': '/cosmos.bank.v1beta1.MsgSend',
+                            amount: [
+                                {
+                                    denom: 'udaric',
+                                    amount: '1100000',
+                                },
+                            ],
+                            to_address: 'desmos1srujv22zfrwyfvu2vyyaqqq3f0z7yjeaggd9n2',
+                            from_address: 'desmos1dzn2s7l0wm9kekyazcnhapu8j95n90efmcmrad',
+                        })]}
+                        viewRaw={false}
+                        toggleMessageDisplay={jest.fn()}
+                        onMessageFilterCallback={jest.fn()}
+                    />
+                </MockTheme>
+            </RecoilRoot>,
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 
-	afterEach(() => {
-		jest.clearAllMocks();
-	});
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 });

1507595c63479bde6a84cd0f5449a4a65f015576
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-11-14 18:02:00 +0300

    move common types to commons.ts

--- src/models/msg/cheqd/commons.ts
+++ src/models/msg/cheqd/commons.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 export class SignInfo {
     public verificationMethodId: string;
     public signature: string;
@@ -25,4 +26,120 @@ export class SignInfo {
       }
       return message;
     }
+}
+
+
+export class Service {
+  public id: string;
+  public type: string;
+  public serviceEndpoint: string;
+
+  constructor(id: string,
+    type: string,
+    serviceEndpoint: string) {
+    this.id = id;
+    this.type = type;
+    this.serviceEndpoint = serviceEndpoint;
+  }
+
+  static fromJson(object: any): Service {
+    const message = { } as Service;
+    if (object.id !== undefined && object.id !== null) {
+      message.id = String(object.id);
+    } else {
+      message.id = '';
+    }
+    if (object.type !== undefined && object.type !== null) {
+      message.type = String(object.type);
+    } else {
+      message.type = '';
+    }
+    if (
+      object.serviceEndpoint !== undefined
+      && object.serviceEndpoint !== null
+    ) {
+      message.serviceEndpoint = String(object.serviceEndpoint);
+    } else {
+      message.serviceEndpoint = '';
+    }
+    return message;
+  }
+}
+export class VerificationMethod {
+public id: string;
+public type: string;
+public controller: string;
+  /** optional */
+  public publicKeyJwk: KeyValuePair[];
+  /** optional */
+  public publicKeyMultibase: string;
+
+  constructor(id: string,
+    type: string,
+    controller: string,
+    publicKeyJwk: KeyValuePair[],
+    publicKeyMultibase: string) {
+    this.type = type;
+    this.controller = controller;
+    this.publicKeyJwk = publicKeyJwk;
+    this.publicKeyMultibase = publicKeyMultibase;
+  }
+  static fromJson(object: any): VerificationMethod {
+    const message = { } as VerificationMethod;
+    message.publicKeyJwk = [];
+    if (object.id !== undefined && object.id !== null) {
+      message.id = String(object.id);
+    } else {
+      message.id = '';
+    }
+    if (object.type !== undefined && object.type !== null) {
+      message.type = String(object.type);
+    } else {
+      message.type = '';
+    }
+    if (object.controller !== undefined && object.controller !== null) {
+      message.controller = String(object.controller);
+    } else {
+      message.controller = '';
+    }
+    if (object.publicKeyJwk !== undefined && object.publicKeyJwk !== null) {
+      for (const e of object.publicKeyJwk) {
+        message.publicKeyJwk.push(KeyValuePair.fromJson(e));
+      }
+    }
+    if (
+      object.publicKeyMultibase !== undefined
+      && object.publicKeyMultibase !== null
+    ) {
+      message.publicKeyMultibase = String(object.publicKeyMultibase);
+    } else {
+      message.publicKeyMultibase = '';
+    }
+    return message;
+  }
+}
+
+class KeyValuePair {
+  public key: string;
+  public value: string;
+
+  constructor(key: string, value: string) {
+    this.key = key;
+    this.value = value;
+  }
+
+  static fromJson(object: any): KeyValuePair {
+    const message = { } as KeyValuePair;
+    if (object.key !== undefined && object.key !== null) {
+      message.key = String(object.key);
+    } else {
+      message.key = '';
+    }
+    if (object.value !== undefined && object.value !== null) {
+      message.value = String(object.value);
+    } else {
+      message.value = '';
+    }
+    return message;
+  }
 }

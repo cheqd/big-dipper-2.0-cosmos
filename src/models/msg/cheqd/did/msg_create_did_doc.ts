8eef359d54144e3db12e0d271e19ec4d3789f3fd
Filip Djokic <87134019+filipdjokic@users.noreply.github.com>
2023-02-23 13:50:13 +0100

    feat: Sync mainnet with testnet [DEV-2199] (#117)
    - Fixed the cheqd module-related labels in detailed transaction views
    - Introduced the correct URL handler for resources (needed to change logic)
    - Fixed DID-related URL handlers
    - Got rid of ts-proto and switched to GraphQL - had to do it to make it consistent
    - Fixed transaction filters in a detailed transaction view

---
+++ src/models/msg/cheqd/did/msg_create_did_doc.ts
@@ -0,0 +1,62 @@
+import {
+  Service, SignInfo, VerificationMethod,
+} from '../commons';
+import { Categories } from '../../types';
+
+export class MsgCreateDidDoc {
+  public category: Categories;
+  public type: string;
+  public json: any;
+  public payload?: MsgCreateDidDocPayload;
+  public signatures: SignInfo[];
+
+  constructor(
+    type: string,
+    json: any,
+    payload: MsgCreateDidDocPayload,
+    signatures: SignInfo[],
+  ) {
+    this.category = 'cheqd';
+    this.type = type;
+    this.json = json;
+    this.payload = payload;
+    this.signatures = signatures;
+  }
+
+  static fromJson(object: any): MsgCreateDidDoc {
+    const message = {} as MsgCreateDidDoc;
+    message.category = 'cheqd';
+    message.signatures = [];
+    message.json = object;
+    message.type = object['@type'];
+
+    if (object.payload !== undefined && object.payload !== null) {
+      message.payload = object.payload;
+    } else {
+      message.payload = undefined;
+    }
+    if (object.signatures !== undefined && object.signatures !== null) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const e of object.signatures) {
+        message.signatures.push(e);
+      }
+    }
+    return message;
+  }
+}
+
+type MsgCreateDidDocPayload = {
+  context: string[];
+  id: string;
+  controller: string[];
+  verificationMethod: VerificationMethod[];
+  authentication: string[];
+  assertionMethod: string[];
+  capabilityInvocation: string[];
+  capabilityDelegation: string[];
+  keyAgreement: string[];
+  alsoKnownAs: string[];
+  service: Service[];
+};
+
+export default MsgCreateDidDoc;

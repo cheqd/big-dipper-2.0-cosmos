da2e4de247366700762b980cfd4c625ce8a1db6a
Filip Djokic <djokicf@protonmail.com>
2023-02-23 13:17:19 +0100

    Update resources URL

--- src/utils/go_to_page.ts
+++ src/utils/go_to_page.ts
@@ -25,4 +25,4 @@ export const ADDRESS_DETAILS = (address: string) => (address.includes(chainConfi
 
 export const DID_RESOLVER_URL = (did: string) => (`https://resolver.cheqd.net/1.0/identifiers/${did}`);
 
-export const RESOURCE_URL = (collection: string, id: string) => (`https://resolver.cheqd.net/1.0/identifiers/did:cheqd:${method}:${collection}/resources/${id}/metadata`);
+export const RESOURCE_URL = (collection: string, id: string) => (`https://resolver.cheqd.net/1.0/identifiers/did:cheqd:${method}:${collection}/resources/${id}`);
